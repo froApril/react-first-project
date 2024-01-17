01d8018385d08543887810ca6067621d265d97d6
FroApril <188494506@qq.com>
2024-01-17 21:13:21 +1100

    Create a generic data fetching hook

--- game-hub/src/hooks/useGames.ts
+++ game-hub/src/hooks/useGames.ts
@@ -1,13 +1,10 @@
-import { useState, useEffect } from "react";
-import apiClients from "../services/api-clients";
-import { CanceledError } from "axios";
+import useData from "./useData";
 
 export interface Platform {
     id: number;
     name: string;
     slug: string;
 }
-
 
 export interface Game {
     id: number;
@@ -16,36 +13,8 @@ export interface Game {
     parent_platforms: {platform: Platform}[],
     metacritic: number,
 }
-  
-interface FeatchGamesResponse {
-    count: number;
-    results: Game[];
-}
 
-const useGames = ()=> {
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setError] = useState("");
 
-    const [isLoading, setLoading] = useState(false);
-  
-    useEffect(() => {
-      const controller = new AbortController();
-      setLoading(true);
-      apiClients
-        .get<FeatchGamesResponse>("/games", { signal: controller.signal})
-        .then((res) => {
-            setGames(res.data.results)
-            setLoading(false);
-        })
-        .catch((err) => {
-            if (err instanceof CanceledError) return;
-            setError(err.message)
-            setLoading(false);
-        });
-      return ()=> controller.abort();
-    }, []);
-
-    return {games, error, isLoading};
-}
+const useGames = ()=> useData<Game>('/games')
 
 export default useGames;
